4d4e50bfcd5dd2e1da73b79811702a1ade148760
Alexandre Marques Bulcao <ambulcao@gmail.com>
2021-02-19 21:30:51 +0000

    Ajustando SideBar Mobile Icon

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { SidebarConteiner, Icon, CloseIcon, SidebarWrapper, SidebarLink, SidebarRoute, SidebarMenu, SidebarbtnWrap } from './SidebarElements';
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
     return (
-        <>
-            
-        </>
-    )
-}
+        <SidebarConteiner isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />    
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="sobre">Sobre</SidebarLink>
+                    <SidebarLink to="explore">Explore</SidebarLink>
+                    <SidebarLink to="servicos">Serviços</SidebarLink>
+                    <SidebarLink to="inscrever">Inscrever-se</SidebarLink>
+                </SidebarMenu>
+                <SidebarbtnWrap>
+                    <SidebarRoute to="/entre">Entre</SidebarRoute>
+                </SidebarbtnWrap> 
+            </SidebarWrapper>    
+        </SidebarConteiner>
+    );
+};
 
 export default Sidebar;
